b362f674712928d4e9e18caaadd1bc6452f7ca5d
kadmokaja <kadmohardy@gmail.com>
2019-07-11 18:10:56 -0300

    CREATE MOUDULO 7

---
+++ modulo07/rocketshoes/src/pages/Home/index.js
@@ -0,0 +1,107 @@
+import React from "react";
+import { MdAddShoppingCart } from "react-icons/md";
+
+import { ProductList } from "./styles";
+
+export default function Home() {
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-kyrie-flytrap-ii-masculino/20/HZM-1289-120/HZM-1289-120_detalhe2.jpg?resize=326:*"
+          alt="Tenis"
+        />
+        <strong>Tênis Nike Kyrie</strong>
+        <span>R$229,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-kyrie-flytrap-ii-masculino/20/HZM-1289-120/HZM-1289-120_detalhe2.jpg?resize=326:*"
+          alt="Tenis"
+        />
+        <strong>Tênis Nike Kyrie</strong>
+        <span>R$229,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-kyrie-flytrap-ii-masculino/20/HZM-1289-120/HZM-1289-120_detalhe2.jpg?resize=326:*"
+          alt="Tenis"
+        />
+        <strong>Tênis Nike Kyrie</strong>
+        <span>R$229,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-kyrie-flytrap-ii-masculino/20/HZM-1289-120/HZM-1289-120_detalhe2.jpg?resize=326:*"
+          alt="Tenis"
+        />
+        <strong>Tênis Nike Kyrie</strong>
+        <span>R$229,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-kyrie-flytrap-ii-masculino/20/HZM-1289-120/HZM-1289-120_detalhe2.jpg?resize=326:*"
+          alt="Tenis"
+        />
+        <strong>Tênis Nike Kyrie</strong>
+        <span>R$229,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-kyrie-flytrap-ii-masculino/20/HZM-1289-120/HZM-1289-120_detalhe2.jpg?resize=326:*"
+          alt="Tenis"
+        />
+        <strong>Tênis Nike Kyrie</strong>
+        <span>R$229,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
+}
